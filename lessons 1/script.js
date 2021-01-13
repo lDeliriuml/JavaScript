6eb24170bf4597a1152b827d9ec5b5e2d9b84dee
Denis <vikulov-dv@avid.ru>
2021-01-13 14:56:32 +0500

    Это первый комит

---
+++ lessons 1/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// type variable
+var number = 5; // Infinity (4 / 0), NaN (Not a Number ('string' * 9))
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true; // false
+null; // переменной не существует (обычно выдается ошибка)
+undefined; // неинициализированная переменная
+var obj = {};
